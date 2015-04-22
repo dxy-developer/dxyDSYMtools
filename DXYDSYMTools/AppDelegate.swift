fdb82019525b8fad31853e02c0fa691a174b9644
wangrui <wangrui@dxyer.com>
2015-04-22 11:20:28 +0800

    Initial Commit

---
+++ DXYDSYMTools/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  DXYDSYMTools
+//
+//  Created by HamGuy on 4/22/15.
+//  Copyright (c) 2015 HamGuy. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
